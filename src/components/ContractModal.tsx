41c31d5771251e1681b4ac84ede04bd6fcff2b97
Nico Serrano <nnicosp@hotmail.com>
2022-08-05 15:20:28 -0400

    feat: handle change input

--- src/components/ContractModal.tsx
+++ src/components/ContractModal.tsx
@@ -15,13 +15,21 @@ function ContractModal(props: any) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [inputs, setInputs] = useState({});
+  function changeInput(e: any){
+    console.log(e.target.value)
+    console.log(e.target.id)
+    let newInput: any = inputs;
+    newInput[e.target.value] = e.target.id;
+    setInputs(newInput);
+  }
   function createInputs(n: Number){
     let content = [];
     for (var i=0; i<n; i++){
       content.push(
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>{`Key ${i+1}:`}</Form.Label>
-          <Form.Control type="text" placeholder="Input your public or public address" />
+          <Form.Control id={i.toString()} onChange={changeInput} type="text" placeholder="Input your public or public address" />
         </Form.Group>
       );
     }
@@ -40,13 +48,140 @@ function ContractModal(props: any) {
   }, []);
   async function compileAndDeploy () {
 
-    const contractCode = `// SPDX-License-Identifier: MIT
-    pragma solidity ^0.8.0;
-    contract FinalWallet {
-        function helloWorld() public pure returns (string memory) {
-            return "Hello, World!";
+    const contractCode = `
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.15;
+
+    abstract contract AbstractWallet {
+        uint nonce;
+        mapping (address => uint) keys;
+
+        constructor(address[] memory pubKeys) {
+            for (uint i = 0; i < pubKeys.length; i++) {
+                keys[pubKeys[i]] = i+1;
+            }
         }
-    }`;
+
+        receive() external payable { }
+
+        function getNonce() public view returns (uint) {
+            return nonce;
+        }
+
+        function isAuthorized(bytes32 msgHash, bytes[] calldata signatures) internal virtual returns (bool);
+
+        function requireAuthorized(bytes32 msgHash, bytes[] calldata signatures) internal {
+            require(isAuthorized(msgHash, signatures), "Not Authorized");
+        }
+
+        function transfer(address payable destination, uint amount, bytes[] calldata signatures) external {
+            uint n  = nonce;
+            bytes32 msgHash = hashTransfer(n, destination, amount);
+            requireAuthorized(msgHash, signatures);
+            nonce = n + 1;
+            destination.transfer(amount);
+        }
+
+        function hashTransfer(uint n, address destination, uint amount) public pure returns (bytes32) {
+            return keccak256(abi.encodePacked("transfer", n, destination, amount));
+        }
+
+        function call(address dest, bytes calldata cd, uint amount, bytes[] calldata signatures) external {
+            uint n  = nonce;
+            bytes32 msgHash = hashCall(n, dest, cd, amount);
+            requireAuthorized(msgHash, signatures);
+            nonce = n + 1;
+            (bool success, bytes memory errMsg) = dest.call{value:amount}(cd);
+            require(success, string(errMsg));
+        }
+
+        function hashCall(uint n, address destination, bytes calldata cd, uint amount) public pure returns (bytes32) {
+            return keccak256(abi.encodePacked("call", n, destination, cd, amount));
+        }
+
+        function replace(address originalKey, address newKey, bytes[] calldata signatures) external {
+            uint n  = nonce;
+            bytes32 msgHash = hashReplace(n, originalKey, newKey);
+            requireAuthorized(msgHash, signatures);
+            nonce = n + 1;
+            replaceOne(originalKey, newKey);
+        }
+
+        function replaceOne(address originalKey, address newKey) internal {
+            uint originalPos = keys[originalKey];
+            require(originalPos != 0, "Not an original key");
+            require(keys[newKey] == 0, "Not an new key");
+            keys[newKey] = originalPos;
+            keys[originalKey] = 0;
+        }
+
+        function hashReplace(uint n, address a, address b) public pure returns (bytes32) {
+            return keccak256(abi.encodePacked("replace", n, a, b));
+        }
+
+        function rotate(address[] memory originalKeys, address[] memory newKeys, bytes[] calldata signatures) external {
+            require(originalKeys.length == newKeys.length, "keys must have same length");
+            uint n  = nonce;
+            bytes32 msgHash = hashRotate(n, originalKeys, newKeys);
+            requireAuthorized(msgHash, signatures);
+            nonce = n + 1;
+            for(uint i=0;i<originalKeys.length;i++) {
+                replaceOne(originalKeys[i], newKeys[i]);
+            }
+        }
+
+        function hashRotate(uint n, address[] memory a, address[] memory b) public pure returns (bytes32) {
+            return keccak256(abi.encodePacked("rotate", n, a, b));
+        }
+
+        function recoverSig(bytes calldata sig, bytes32 msgHash) internal pure returns (address) {
+            uint8 v = uint8(sig[64]);
+            uint src;
+            assembly {
+                src := sig.offset
+            }
+            bytes32 r;
+            assembly {
+                r := calldataload(src)
+            }
+            src += 0x20;
+            bytes32 s;
+            assembly {
+                s:= calldataload(src)
+            }
+            return ecrecover(msgHash, v, r, s);
+        }
+    }
+
+    abstract contract BoolWallet is AbstractWallet {
+        uint total;
+        constructor(address[] memory pubKeys) AbstractWallet(pubKeys) {
+            total = pubKeys.length;
+        }
+
+        function isAuthorized(bytes32 msgHash, bytes[] calldata signatures) internal view override returns (bool) {
+            bool[] memory hasSig = new bool[](total);
+            for(uint i = 0; i < signatures.length; i++) {
+                uint pos = keys[recoverSig(signatures[i], msgHash)];
+                if (pos > 0) {
+                    hasSig[pos - 1] = true;
+                }
+            }
+            return boolAuthorized(hasSig);
+        }
+
+        function boolAuthorized(bool[] memory hasSig) internal view virtual returns (bool);
+    }
+
+    contract SampleThreeWallet is BoolWallet {
+        constructor(address[] memory pubKeys) BoolWallet(pubKeys) {
+        }
+
+        function boolAuthorized(bool[] memory k) internal pure override returns (bool) {
+            return (k[0] && k[1]) || k[2];
+        }
+    }
+    `;
     /*
     const booleanFormula = roiCode();
     function boolAuthorized(bool[] memory k) internal pure override returns (bool){
